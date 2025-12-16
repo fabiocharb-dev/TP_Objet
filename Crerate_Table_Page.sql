004f11e442acd79ce1f3727051f1b37012d70c12
Ichiko <fabiocharb@gmail.com>
2025-12-16 14:45:29 +0100

    Delete Crerate_Table_Page.sql

--- Crerate_Table_Page.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tp`.`Page` 
-(`id` INT NULL ,
- `Nom` VARCHAR(400) NOT NULL ,
-  `Contenu` VARCHAR(1500) NOT NULL ,
-   `Date` DATE NOT NULL ,
-    UNIQUE (`id`)
-   ) ENGINE = InnoDB;
