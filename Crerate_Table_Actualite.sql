43cc5657b1d0b0cb7b593677ffc360238111e8ee
Ichiko <fabiocharb@gmail.com>
2025-12-16 14:37:35 +0100

    Update Crerate_Table_Actualite.sql

--- Crerate_Table_Actualite.sql
+++ Crerate_Table_Actualite.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `tp`.`actualite` 
+CREATE TABLE `tp`.`Actualite` 
 (`id` INT NULL ,
  `Nom` VARCHAR(400) NOT NULL ,
   `Contenu` VARCHAR(1500) NOT NULL ,
    `Date` DATE NOT NULL , UNIQUE (`id`)
+
    ) ENGINE = InnoDB;
